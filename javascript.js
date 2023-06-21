b66da36a321299060b3541014b4f0264ea58532f
szymon-kowal <00szymon.kowal@gmail.com>
2023-06-21 16:09:36 +0200

    Add function that simulat one round of game and for loop to have 5 games

--- javascript.js
+++ javascript.js
@@ -1,14 +1,34 @@
 console.log(getComputerChoice())
 
-let playerChoice = toLowerCase() 
+let playerChoice = prompt('Enter your choice. Rock/Paper/Scissors.') 
+playerChoice = playerChoice.toLowerCase()
+
+for (let i = 0; i < 5; i++) {
+    console.log(oneRound())
+}
+
+function oneRound() {
+    computerSelection = getComputerChoice();
+    if (playerChoice === computerSelection) {
+        return "It is a tie!";
+    }  else if ( playerChoice === "paper" && computerSelection === "scissors") {
+        return " You have lost !"
+    } else if ( playerChoice === "scissors" && computerSelection === "rock") {
+        return " You have lost !"
+    } else if ( playerChoice === "rock" && computerSelection === "paper") {
+        return " You have lost !"
+    } else {
+        return " You have won !!!"
+    }
+}
 
 function getComputerChoice() {
-    let random = Math.floor(Math.random() * 3) + 1;
+    const random = Math.floor(Math.random() * 3) + 1;
     if (random === 1) {
-        return "Scissors";
+        return "scissors";
     } else if (random === 2) {
-        return "Rock";
+        return "rock";
     } else {
-        return "Paper";
+        return "paper";
     }  
 }
