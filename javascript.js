ba29cf7e88b86968ed75bd6476e399fb466fd709
szymon-kowal <00szymon.kowal@gmail.com>
2023-06-25 14:12:12 +0200

    Add switching score on web page.

--- javascript.js
+++ javascript.js
@@ -8,17 +8,36 @@ let whoWon;
 const buttons = document.querySelectorAll('button');
 console.log(buttons)
 
+const outputPlayer = document.querySelector('.p');
+const outputComputer = document.querySelector('.c');
+const outputWinner = document.querySelector('.winner');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         if (button.classList.contains('rock')) {
             console.log("Rock button was clicked.");
+            let playerChoice = 'rock';
+            oneRound(playerChoice,getComputerChoice);
           } else if (button.classList.contains('paper')) {
             console.log("Paper button was clicked.");
+            let playerChoice = 'paper';
+            oneRound(playerChoice,getComputerChoice);
           } else if (button.classList.contains('scissors')) {
             console.log("Scissors button was clicked.");
-          }       
+            let playerChoice = 'scissors';
+            oneRound(playerChoice,getComputerChoice);
+          } 
+          outputComputer.textContent = `${computerWin}`;
+          outputPlayer.textContent = `${playerWin}`;
+          outputWinner.textContent = `placeholder`;
+
+          // Need to keep the progress of game to the 5 wins and
+          // make the winner     
     })
 });
+
+if (playerWin = 5) {
+    
+}
 
 // let playerChoice = prompt('Enter your choice. Rock/Paper/Scissors.')
 // playerChoice = playerChoice.toLowerCase()
