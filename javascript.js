193e8933930fcd20bc3f6cc4b3ce6a07a56edf43
szymon-kowal <00szymon.kowal@gmail.com>
2023-06-27 19:34:50 +0200

    Relocate everything on web page.

--- javascript.js
+++ javascript.js
@@ -12,11 +12,11 @@ const blinking = document.getElementsByClassName('blink')[0];
 let index = 0;
 let index2 = 0;
 
-function addText(callback) {
+function addText() {
     textElement.innerHTML += text.charAt(index);
     index++;
     if (index < text.length) {
-        setTimeout(addText, 30);
+        setTimeout(addText, 3);
     } else {
         blinking.innerHTML ="";
         setTimeout(addEnterText,1000);
@@ -28,16 +28,20 @@ function addEnterText() {
     textEnterElement.innerHTML += textEnter.charAt(index2);
     index2++;
     if (index2 < textEnter.length) {
-        setTimeout(addEnterText, 25);
+        setTimeout(addEnterText, 2);
     } 
 }
 
 setTimeout(addText, 3000);
 
 const overl = document.getElementsByClassName('overlay')[0];
+const cont = document.getElementsByClassName('container')[0];
 
+let hasRun = false;
 function checkKeyPressed(e) {
-    if (e.key === 'Enter') {
+    if (e.key === 'Enter' && hasRun == false) {
         overl.innerHTML = '';
+        cont.id = '';
+        hasRun = true;
     }
 };
